f79c3ca591492c5d47cb3207b5576a96de37411a
hovanhoa <hovanhoaa1@gmail.com>
2024-09-17 17:09:24 +0700

    feat: remove cache

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,7 +15,6 @@ async function getPosts(
     headers: {
       "Content-Type": "application/json",
     },
-    cache: "no-store",
     body: JSON.stringify({ query: GET_POST }),
     next: {
       revalidate: 1,
