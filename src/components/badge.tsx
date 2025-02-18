aa05bc39286284fa2b2ea2b164db04ed4c063bfc
hovanhoa <hovanhoaa1@gmail.com>
2025-02-18 13:06:13 +0700

    feat: update Testing

--- src/components/badge.tsx
+++ src/components/badge.tsx
@@ -28,7 +28,7 @@ export default function Badge({
                 return 'bg-purple-100 text-purple-800 border-purple-200'
             case 'System Design':
                 return 'bg-yellow-100 text-yellow-800 border-yellow-200'
-            case 'testing':
+            case 'Testing':
                 return 'bg-green-100 text-green-800 border-green-200'
         }
     }
