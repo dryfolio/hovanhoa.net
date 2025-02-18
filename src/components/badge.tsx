7059fb0a4dd3476fafb230a5d0f00c2961ef8b1e
hovanhoa <hovanhoaa1@gmail.com>
2025-02-18 11:35:40 +0700

    feat: update badge for testing

--- src/components/badge.tsx
+++ src/components/badge.tsx
@@ -28,6 +28,8 @@ export default function Badge({
                 return 'bg-purple-100 text-purple-800 border-purple-200'
             case 'System Design':
                 return 'bg-yellow-100 text-yellow-800 border-yellow-200'
+            case 'testing':
+                return 'bg-green-100 text-green-800 border-green-200'
         }
     }
 
