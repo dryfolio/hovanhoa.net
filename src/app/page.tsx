76c54e889a2f777a15923d54c9fc5ed3fa4a32ae
hovanhoa <hovanhoaa1@gmail.com>
2025-04-12 10:33:13 +0700

    feat: add X metadata

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,35 @@ import Link from 'next/link'
 import { Footer } from '@/components/footer'
 import { unstable_noStore as noStore } from 'next/cache'
 import { HashNode } from '@/lib/hashnode'
+import { Metadata } from 'next'
+import { BASE_URL, NAME, ROLE, IMAGE } from '@/constants'
+
+export const metadata: Metadata = {
+    title: NAME,
+    description: ROLE,
+    openGraph: {
+        title: NAME,
+        description: ROLE,
+        url: BASE_URL,
+        siteName: NAME,
+        images: [
+            {
+                url: IMAGE,
+                width: 800,
+                height: 600,
+            },
+        ],
+        locale: 'en_US',
+        type: 'website',
+    },
+    twitter: {
+        card: 'summary_large_image',
+        title: NAME,
+        description: ROLE,
+        creator: '@_hovanhoa_',
+        images: [IMAGE],
+    },
+}
 
 export default async function Home() {
     noStore()
